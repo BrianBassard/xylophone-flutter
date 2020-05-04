ab097e703bf1c1bfc8a82d09a83cd1334d595f79
MrBlackTie <brianbassard@gmail.com>
2020-05-04 13:11:04 -0700

    Button Boilerplate
    Added 'Do' in next octave

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -6,10 +7,85 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
-        ),
+            child: Column(
+          children: <Widget>[
+            FlatButton(
+              onPressed: () {
+                final assetsAudioPlayer = AssetsAudioPlayer();
+                assetsAudioPlayer.open(
+                  Audio('assets/audios/note1.wav'),
+                );
+              },
+              child: Text('Do'),
+            ),
+            FlatButton(
+              onPressed: () {
+                final assetsAudioPlayer = AssetsAudioPlayer();
+                assetsAudioPlayer.open(
+                  Audio('assets/audios/note2.wav'),
+                );
+              },
+              child: Text('Re'),
+            ),
+            FlatButton(
+              onPressed: () {
+                final assetsAudioPlayer = AssetsAudioPlayer();
+                assetsAudioPlayer.open(
+                  Audio('assets/audios/note3.wav'),
+                );
+              },
+              child: Text('Mi'),
+            ),
+            FlatButton(
+              onPressed: () {
+                final assetsAudioPlayer = AssetsAudioPlayer();
+                assetsAudioPlayer.open(
+                  Audio('assets/audios/note4.wav'),
+                );
+              },
+              child: Text('Fa'),
+            ),
+            FlatButton(
+              onPressed: () {
+                final assetsAudioPlayer = AssetsAudioPlayer();
+                assetsAudioPlayer.open(
+                  Audio('assets/audios/note5.wav'),
+                );
+              },
+              child: Text('So'),
+            ),
+            FlatButton(
+              onPressed: () {
+                final assetsAudioPlayer = AssetsAudioPlayer();
+                assetsAudioPlayer.open(
+                  Audio('assets/audios/note6.wav'),
+                );
+              },
+              child: Text('La'),
+            ),
+            FlatButton(
+              onPressed: () {
+                final assetsAudioPlayer = AssetsAudioPlayer();
+                assetsAudioPlayer.open(
+                  Audio('assets/audios/note7.wav'),
+                );
+              },
+              child: Text('Ti'),
+            ),
+            FlatButton(
+              onPressed: () {
+                final assetsAudioPlayer = AssetsAudioPlayer();
+                assetsAudioPlayer.open(
+                  Audio('assets/audios/note8.wav'),
+                );
+              },
+              child: Text('Do'),
+            ),
+          ],
+        )),
       ),
     );
   }
